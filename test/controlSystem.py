042ae15e796c021401bf4f5a24f9488d6eb991b1
stmcmurry <60404106+stmcmurry@users.noreply.github.com>
2020-10-26 17:01:37 -0400

    Add files via upload

---
+++ test/controlSystem.py
@@ -0,0 +1,176 @@
+"""
+This is the progrom that should be run in order to control the Youcan submarine.
+The file is meant to work in tandem with scrcpy
+"""
+
+import subMovement
+import tkinter as tk
+from tkinter import ttk
+
+#a method to use the Submit button with the 'enter' key
+def onclick(event=None):
+    answer = entry.get()
+    splitString(answer)
+    
+#a method to get a value for how long the sub moves
+def splitString(answer):
+    if ',' in answer:
+        blockCommand(answer)
+    else:
+        msg = [0]*2
+        print(msg)
+        msg = answer.split(' ')
+        msg.append('0')
+        print(msg)
+        switch(msg[0], msg[1])    
+    
+#a method to display a popup window if the user inputs an unrecognized command    
+def popupmsg(msg):
+    popup = tk.Tk()
+    popup.wm_title("Command Not Recognized")
+    label = ttk.Label(popup, text=msg)
+    label.pack(side="top", fill="x", pady=10)
+    B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
+    B1.pack()
+    popup.mainloop()
+
+def blockCommand(answer):
+    block = answer.split(' ')
+    
+    for i in range(len(block)):
+        if ',' in block[i]:
+            block[i] = block[i].replace(",", "")
+            
+    for j in range(len(block)):
+        if block[j].isnumeric() or block[j+1].isnumeric():
+            continue
+        else:
+            block.insert(j+1, "0")
+            
+    if len(block) % 2 == 0:
+        for k in range(len(block)):
+            if k % 2 == 1:
+                switch(block[k-1], block[k])
+            else:
+                continue
+            
+    elif len(block) % 2 != 0:
+        block.append("0")
+        for k in range(len(block)):
+            if k % 2 == 1:
+                switch(block[k-1], block[k])
+            else:
+                continue
+    print(block)
+    
+#a switching method that contains the keywords to control the submarine      
+def switch(choice, time):
+    if choice.upper() == "LEFT":
+        sub = subMovement.subMovement()
+        int(time)
+        sub.moveLeft(time)
+    
+    elif choice.upper() == "RIGHT":
+        sub = subMovement.subMovement()
+        int(time)
+        sub.moveRight(time)
+        
+    elif choice.upper() == "FORWARD":
+        sub = subMovement.subMovement()
+        int(time)
+        sub.moveForward(time)
+        
+    elif choice.upper() == "BACK":
+        sub = subMovement.subMovement()
+        int(time)
+        sub.moveBackward(time)
+        
+    elif choice.upper() == "UP":
+        sub = subMovement.subMovement()
+        int(time)
+        sub.moveUp(time)
+       
+    elif choice.upper() == "DOWN":
+        sub = subMovement.subMovement()
+        int(time)
+        sub.moveDown(time)
+        
+    elif choice.upper() == "RECORD":
+        sub = subMovement.Camera()
+        sub.recordVideo()
+        
+    elif choice.upper() == "PICTURE" or "PHOTO":
+        sub = subMovement.Camera()
+        sub.takePhoto()
+        
+    elif choice.upper() == "STABLE":
+        sub = subMovement.Camera()
+        sub.stabilizeImage()
+        
+    elif choice.upper() == "DEPTH":
+        sub = subMovement.Camera()
+        sub.depthMaintain()
+        
+    elif choice.upper() == "LOW":
+        sub = subMovement.subMovement()
+        sub.speedUp(choice)
+        
+    elif choice.upper() == "MEDIUM":
+        sub = subMovement.subMovement()
+        sub.speedUp(choice)
+        
+    elif choice.upper() == "HIGH":
+        sub = subMovement.subMovement()
+        sub.speedUp(choice)
+        
+    elif choice.upper() == "LOCK":
+        sub = subMovement.Camera()
+        sub.lock()
+        
+    elif choice.upper() == "UNLOCK":
+        sub = subMovement.Camera()
+        sub.unlock()
+        
+    elif choice.upper() == "DIM":
+        sub = subMovement.Camera()
+        sub.lights(choice)
+        
+    elif choice.upper() == "MID":
+        sub = subMovement.Camera()
+        sub.lights(choice)
+        
+    elif choice.upper() == "BRIGHT":
+        sub = subMovement.Camera()
+        sub.lights(choice)
+        
+    else:
+        popupmsg("The command you selected does not exist. Please try another command.")
+        
+top = tk.Tk()
+camera = subMovement.Camera()
+
+top.bind('<Return>', onclick)
+
+bwidth = 22
+bheight = 3
+
+title = tk.Label(text = "Submarine Control System")
+
+#entry widget to input command words
+entry = tk.Entry(top,
+                 bg = "white")
+#button for submitting a command
+submit = tk.Button(
+    text = "Submit",
+    width = 10,
+    height = 2,
+    bg = "black",
+    fg = "white",
+    command = onclick
+)
+
+title.pack()
+submit.pack(side = tk.BOTTOM)
+entry.pack(side = tk.BOTTOM)
+
+top.mainloop()
---
+++ test/subMovement.py
@@ -0,0 +1,162 @@
+"""
+Senior Design submarine methods for submarine movement and control
+"""
+import pyautogui
+import time
+from pynput.mouse import Button, Controller
+
+"""
+A program that defines two classes to control the Youcan submarine.
+This is meant to be used with scrcpy.
+Also use the following commands in the Anaconda command terminal:
+    pip install pyautogui
+    pip install pynput
+"""
+
+#This class controls the basic submarine movements and speed
+class subMovement:
+    def __init__(self):
+        self.speed = "MEDIUM"
+    
+    #A method to make the submarine move left
+    def moveLeft(self, duration):
+        pyautogui.moveTo(344,700, duration = 0)
+        mouse = Controller()
+        mouse.press(Button.left)
+        pyautogui.dragTo(221, 700, duration=1.5)
+        time.sleep(duration)
+        mouse.release(Button.left)
+        backToGUI()
+
+    #A method to make the submarine move right
+    def moveRight(self, duration):
+        pyautogui.moveTo(344,700, duration = 0)
+        mouse = Controller()
+        mouse.press(Button.left)
+        pyautogui.dragTo(481, 700, duration=1.5)
+        time.sleep(duration)
+        mouse.release(Button.left)
+        backToGUI()
+    
+    #A method to make the submarine move forward
+    def moveForward(self, duration):
+        pyautogui.moveTo(344,700, duration = 0)
+        mouse = Controller()
+        mouse.press(Button.left)
+        pyautogui.dragTo(344, 580, duration=1.5)
+        time.sleep(duration)
+        mouse.release(Button.left)
+        backToGUI()
+    
+    #A method to make the submarine move backward
+    def moveBackward(self, duration):
+        pyautogui.moveTo(344,700, duration = 0)
+        mouse = Controller()
+        mouse.press(Button.left)
+        pyautogui.dragTo(344, 850, duration=1.5)
+        time.sleep(duration)
+        mouse.release(Button.left)
+        backToGUI()
+    
+    #A method to make the submarine move up
+    def moveUp(self, duration):
+        pyautogui.moveTo(1491,775, duration = 0)
+        mouse = Controller()
+        mouse.press(Button.left)
+        pyautogui.dragTo(1491, 750, duration=1.5)
+        time.sleep(duration)
+        mouse.release(Button.left)
+        backToGUI()
+    
+    #A method to make the submarine move down
+    def moveDown(self, duration):
+        pyautogui.moveTo(1491,775, duration = 0)
+        mouse = Controller()
+        mouse.press(Button.left)
+        pyautogui.dragTo(1491, 825, duration=1.5)
+        time.sleep(duration)
+        mouse.release(Button.left)
+        backToGUI()
+        
+    #a method that changes speed from low, medium and high
+    def speedUp(self, speed):
+        pyautogui.click(1231, 875)
+        if(speed.upper() == "LOW"):
+            mouse = Controller()
+            mouse.press(Button.left)
+            pyautogui.dragTo(1227, 986, duration=0.3)
+            mouse.release(Button.left)
+        elif(speed.upper() == "MEDIUM"):
+            mouse = Controller()
+            mouse.press(Button.left)
+            pyautogui.dragTo(1227, 832, duration=0.3)
+            mouse.release(Button.left)
+        elif(speed.upper() == "HIGH"):
+            mouse = Controller()
+            mouse.press(Button.left)
+            pyautogui.dragTo(1227, 738, duration=0.3)
+            mouse.release(Button.left)
+        self.speed = speed
+        backToGUI()
+
+#A class that controls camera functionalit and lights  
+class Camera:
+    def __init__(self):
+         self.power = True
+         self.record = False
+         self.picture = False
+         self.maintainDepth = False
+         self.stableImage = False
+     
+    #A method to take a photot with the submarine
+    def takePhoto(self):
+        pyautogui.click(1696, 387)
+        backToGUI()
+        
+    #a method to record video
+    def recordVideo(self):
+        pyautogui.click(1695, 460)
+        backToGUI()
+     
+    #a method to display battery life   
+    def stabilizeImage(self):
+        pyautogui.click(1694, 666)
+        backToGUI()
+     
+    #a method to lock the submarine's controls   
+    def lock(self):
+        pyautogui.click(1638, 114)
+        backToGUI()
+    #a method to unlock the submarine's controls    
+    def unlock(self):
+        pyautogui.moveTo(687,533, duration = 0)
+        mouse = Controller()
+        mouse.press(Button.left)
+        pyautogui.dragTo(1200, 533, duration=0.5)
+        mouse.release(Button.left)
+        backToGUI()
+    
+    #a method to maintain depth    
+    def depthMaintain(self):
+        pyautogui.click(686, 872)
+        backToGUI()
+    
+    #a method to control the lights from dim, mid and bright
+    def lights(self, lights):
+        pyautogui.click(564, 877)
+        time.sleep(0.3)
+        if(lights.upper() == "DIM"):
+            pyautogui.click(560,920)
+        elif(lights.upper() == "MID"):
+            pyautogui.click(560,813)
+        elif(lights.upper() == "BRIGHT"):
+            pyautogui.click(558,708)
+        backToGUI()
+        
+#a method to return the window to the GUI            
+def backToGUI():
+    pyautogui.keyDown('alt')
+    time.sleep(0.1)
+    pyautogui.press('tab')
+    time.sleep(0.1)
+    pyautogui.keyUp('alt')
